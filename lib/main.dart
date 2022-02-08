642140b071769b24794e44cf3888e1f69a31bad9
yarchike <yarchike@gmail.com>
2022-02-08 20:36:34 +0300

    work 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: UserPanel(),
+    ));
 
-class MyApp extends StatelessWidget{
+class UserPanel extends StatefulWidget {
+  const UserPanel({Key? key}) : super(key: key);
+
+  @override
+  State<UserPanel> createState() => _UserPanelState();
+}
+
+class _UserPanelState extends State<UserPanel> {
+  int _count = 0;
+
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.deepOrangeAccent, appBarTheme: const AppBarTheme(color:  Colors.deepOrangeAccent)),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("MartynovApp"),
-          centerTitle: true,
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        title: Text("MartynovUser"),
+        centerTitle: true,
+        backgroundColor: Colors.black45,
+      ),
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsets.only(top: 30.0)),
+                Text("Yaroslav Martnov", style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white
+                ),),
+                Padding(padding: EdgeInsets.only(top: 10.0)),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/marvel.jpg'),
+                  radius: 50,
+                ),
+                Padding(padding: EdgeInsets.only(top: 10.0)),
+                Row(
+                  children: [
+                    Icon(Icons.mail_outline, size: 25,),
+                    Padding(padding: EdgeInsets.only(left: 10.0)),
+                    Text("Yarchike@gmail.com", style: TextStyle(color: Colors.white),)
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(top: 10.0)),
+                Text("Count: $_count", style: TextStyle(color: Colors.white),)
+              ],
+            )
+          ],
         ),
-        body: Container(
-            color: Colors.deepOrangeAccent,
-            child: Text('Martynov'),
-          margin: EdgeInsets.fromLTRB(10.0,15.0,10.0,15.0),
-          padding:  EdgeInsets.fromLTRB(10.0,15.0,10.0,15.0),
-          //Image(image: AssetImage('assets/marvel.jpg')),
-          //Image(image: NetworkImage('https://sport-marafon.ru/upload/iblock/726/mount.jpg'), )
-          //TextButton.icon(onPressed: () {  },  label: Text('Нажми'),icon: Icon(Icons.adb_sharp))
-          //RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.adb_sharp), label: Text('Settings'))
-          //RaisedButton(onPressed: onPressed, child: Text("Нажми"), color: Colors.deepOrangeAccent,)
-          //FlatButton(onPressed: onPressed, child: Text("Нажми на меня"), color: Colors.deepOrangeAccent,),
-          //Icon(Icons.amp_stories_outlined, size: 45, color: Colors.amber,),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('$this');
-          },
-          child: Text("Нажми"),
-          backgroundColor: Colors.deepOrangeAccent,
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.amber,
+        onPressed: (){
+          setState(() {
+            _count++;
+          });
+        },
       ),
     );
   }
 }
-void onPressed(){
-
-}
-
