9fddb5e6d8b7fcf3d7b8fe0b2a31475d8111e63d
yarchike <yarchike@gmail.com>
2022-02-08 18:31:01 +0300

    work 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    home: Text('MartynovApp'),
-  )
-);
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepOrangeAccent, appBarTheme: const AppBarTheme(color:  Colors.deepOrangeAccent)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MartynovApp"),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text("MartynovApp", style: TextStyle(
+              fontSize: 20,
+              color: Colors.red,
+              fontFamily: "GoblinOne"
+          ),),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('$this');
+          },
+          child: Text("Нажми"),
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+      ),
+    );
+  }
+}
 
