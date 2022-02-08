c0485f0bd401cda590bf1710ea28e2474cd8fc4f
yarchike <yarchike@gmail.com>
2022-02-08 19:47:46 +0300

    work 6

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,18 @@ class MyApp extends StatelessWidget{
           title: Text("MartynovApp"),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text("MartynovApp", style: TextStyle(
-              fontSize: 20,
-              color: Colors.red,
-              fontFamily: "GoblinOne"
-          ),),
+        body: Container(
+            color: Colors.deepOrangeAccent,
+            child: Text('Martynov'),
+          margin: EdgeInsets.fromLTRB(10.0,15.0,10.0,15.0),
+          padding:  EdgeInsets.fromLTRB(10.0,15.0,10.0,15.0),
+          //Image(image: AssetImage('assets/marvel.jpg')),
+          //Image(image: NetworkImage('https://sport-marafon.ru/upload/iblock/726/mount.jpg'), )
+          //TextButton.icon(onPressed: () {  },  label: Text('Нажми'),icon: Icon(Icons.adb_sharp))
+          //RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.adb_sharp), label: Text('Settings'))
+          //RaisedButton(onPressed: onPressed, child: Text("Нажми"), color: Colors.deepOrangeAccent,)
+          //FlatButton(onPressed: onPressed, child: Text("Нажми на меня"), color: Colors.deepOrangeAccent,),
+          //Icon(Icons.amp_stories_outlined, size: 45, color: Colors.amber,),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
@@ -29,5 +35,8 @@ class MyApp extends StatelessWidget{
       ),
     );
   }
+}
+void onPressed(){
+
 }
 
